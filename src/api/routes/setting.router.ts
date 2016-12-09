d5e00022bb21d274ab9e5ea073adbe5b29ec9b76
Haru <phamlexuanvinh@gmail.com>
2016-12-10 01:24:23 +0700

    edit

--- src/api/routes/setting.router.ts
+++ src/api/routes/setting.router.ts
@@ -1,4 +1,3 @@
-// đây là vùng import tất cả các modules bên ngoài
 import { Router, Response, Request } from 'express';
 
 import path = require('path');
@@ -30,7 +29,9 @@ export class SettingRouter {
         this.router.route("/settingdelete")
             .post(this.Delete);
         this.router.get('/settingcount',this.getcount);
-        
+        this.router.get('/settingAppName',this.getAppName);
+        this.router.get('/settingAPI',this.getAPI);
+        this.router.get('/settingAppID',this.getAppID);
         return this.router;
     }
 
@@ -56,9 +57,43 @@ export class SettingRouter {
                 res.status(500).send(error.message)
             });
     }
+    private getAppName = (req: Request, res: Response) => {
+        console.log('abcs'+req.query.so+' APPNAME '+req.query.ten);
+        this.settingRepo.getAppName(req.query)
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(error => {
+                console.error(error.message);
+                res.status(500).send(error.message)
+            });
+    }
+    private getAPI = (req: Request, res: Response) => {
+        console.log('abcs'+req.query.so+' API '+req.query.id);
+        debugger;
+        this.settingRepo.getAPI(req.query)
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(error => {
+                console.error(error.message);
+                res.status(500).send(error.message)
+            });
+    }
     private getcount = (req: Request, res: Response) => {
         console.log('abcs'+req.query.so);
-        this.settingRepo.getcount(req.query.so)
+        this.settingRepo.getcount()
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(error => {
+                console.error(error.message);
+                res.status(500).send(error.message)
+            });
+    }
+    private getAppID = (req: Request, res: Response) => {
+        console.log('abcs'+req.query.so);
+        this.settingRepo.getAppID()
             .then(result => {
                 res.status(200).json(result)
             })
@@ -102,110 +137,3 @@ export class SettingRouter {
 
 
 }
-
-
-
-// cấu hình router với url mình muốn (ở đây là /book => localhost:port/api/book)
-// router.route('/book')
-//     //lay het sach, hoac lay sach theo id
-//     .get(getAllBook)
-
-//     //tao 1 book
-//     .post((req, res) => {
-//         bookModel.create(req.body).then(b => {
-//             res.json(b);
-//         })
-//             .catch(error => {
-//                 res.status(500).send(error.message);
-//             })
-//     })
-
-//     //sua 1 sach theo id
-//     .put((req, res) => {
-//         let id = req.query.id;
-//         bookModel.findById(id).then(b => {
-//             if (b) {
-//                 return b.update(req.body);
-//             } else {
-//                 return null;
-//             }
-
-//         })
-//             .then(b => {
-//                 if (b) {
-//                     res.json(b);
-//                 } else {
-//                     res.status(404).send('khong tim thay sach voi id =' + id);
-//                 }
-//             })
-//             .catch(error => {
-//                 res.status(500).send(error.message);
-//             })
-//     })
-
-//     //delete a book by id
-//     .delete((req, res) => {
-//         bookModel.findById(req.query.id).then(b => {
-//             if (b) {
-//                 b.destroy();
-//                 return b.name;
-//             } else {
-//                 return null;
-//             }
-
-//         })
-//             .then((v) => {
-//                 if (v) {
-//                     res.status(200).send('sach ' + v + ' da duoc huy');
-//                 } else {
-//                     res.status(404).send('khong tim thay sach');
-//                 }
-
-//             })
-//             .catch(error => {
-//                 res.status(500).send(error.message);
-//             })
-//     });
-
-// // route test quan he        
-// router.route('/bookAuthor')
-
-//     // lay tat ca author cua sach
-//     .get((req, res) => {
-//         let id = req.query.id;
-//         bookModel.findById(id).then(book => {
-//             if (book) {
-//                 book.getAuthors().then(authors => {
-//                     res.json(authors);
-//                 })
-//             }
-//             else {
-//                 res.status(404).send('khong tim thay book voi id=' + id);
-//             }
-//         })
-//             .catch(error => {
-//                 res.status(500).send(error.message);
-//             })
-//     })
-
-
-//     // tao them author cho sach
-//     .put((req, res) => {
-//         let id = req.query.id;
-//         bookModel.findById(id).then(book => {
-//             if (book) {
-//                 let authorName = req.body.name;
-//                 book.createAuthor({ name: authorName })
-//                 res.status(200).send('da tao them author vao book');
-//             } else {
-//                 res.status(404).send('khong tim thay book voi id=' + id);
-//             }
-//         })
-//             .catch(error => {
-//                 res.status(500).send(error.message);
-//             })
-//     })
-
-
-
-
