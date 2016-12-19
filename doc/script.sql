789c7a57d762f23396c1df2ae512fe43f5e0aff2
chuongtv <chuongtovan@gmail.com>
2016-12-19 17:39:29 +0700

    Update script.sql

--- doc/script.sql
+++ doc/script.sql
@@ -12,7 +12,21 @@ Database		PostgreSQL 8.1
 
 /* Create Tables */
 
-
+/*
+chuong tv chinh sua o day
+chuong tv chinh sua o day
+chuong tv chinh sua o day
+*/
+Create table "Log"
+(
+	"IDLog" Serial NOT NULL,
+	"NgayTao" Timestamp,
+	"TextLog" Text,
+	"Platforfrfrm" Varchar(250),
+	"UngDung" Varchar(250),
+	"TieuDeLog" Varchar(250),
+ primary key ("IDLog")
+) Without Oids;
 /*
 chuong tv chinh sua o day
 chuong tv chinh sua o day
