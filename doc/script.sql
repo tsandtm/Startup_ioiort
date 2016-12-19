18c861513637f4d0be1e02c12e41b054ba2efe2e
chuongtv <chuongtovan@gmail.com>
2016-12-19 17:47:25 +0700

    Update script.sql

--- doc/script.sql
+++ doc/script.sql
@@ -72,3 +72,43 @@ Author
 Version		
 Database		PostgreSQL 8.1 
 */
+/*
+Created		20-Oct-16
+Modified		08-Dec-16
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
+*/
+/*
+Created		20-Oct-16
+Modified		08-Dec-16
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
+*/
+/*
+Created		20-Oct-16
+Modified		08-Dec-16
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
+*/
+/*
+Created		20-Oct-16
+Modified		08-Dec-16
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
+*/
