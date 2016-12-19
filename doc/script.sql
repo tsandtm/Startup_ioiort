6e5f4edd97a69834300e78aa8cba927dffa1f2e9
chuongtv <chuongtovan@gmail.com>
2016-12-19 17:47:06 +0700

    Update script.sql

--- doc/script.sql
+++ doc/script.sql
@@ -13,70 +13,62 @@ Database		PostgreSQL 8.1
 /* Create Tables */
 
 /*
-chuong tv chinh sua o day
-chuong tv chinh sua o day
-chuong tv chinh sua o day
+Created		20-Oct-16
+Modified		08-Dec-16
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
 */
-Create table "Log"
-(
-	"IDLog" Serial NOT NULL,
-	"NgayTao" Timestamp,
-	"TextLog" Text,
-	"Platforfrfrm" Varchar(250),
-	"UngDung" Varchar(250),
-	"TieuDeLog" Varchar(250),
- primary key ("IDLog")
-) Without Oids;
 /*
-chuong tv chinh sua o day
-chuong tv chinh sua o day
-chuong tv chinh sua o day
+Created		20-Oct-16
+Modified		08-Dec-16
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
 */
-Create table "Log"
-(
-	"IDLog" Serial NOT NULL,
-	"NgayTao" Timestamp,
-	"TextLog" Text,
-	"Platforfrfrm" Varchar(250),
-	"UngDung" Varchar(250),
-	"TieuDeLog" Varchar(250),
- primary key ("IDLog")
-) Without Oids;
-
-
 /*
-chuong tv chinh sua o day
-chuong tv chinh sua o day
-chuong tv chinh sua o day
+Created		20-Oct-16
+Modified		08-Dec-16
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
 */
-Create table "Log"
-(
-	"IDLog" Serial NOT NULL,
-	"NgayTao" Timestamp,
-	"TextLog" Text,
-	"Platforfrfrm" Varchar(250),
-	"UngDung" Varchar(250),
-	"TieuDeLog" Varchar(250),
- primary key ("IDLog")
-) Without Oids;
 /*
-chuong tv chinh sua o day
-chuong tv chinh sua o day
-chuong tv chinh sua o day
+Created		20-Oct-16
+Modified		08-Dec-16
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
 */
-
-Create table "Log"
-(
-	"IDLog aa" Serial NOT NULL,
-		"NgayTao" Timestamp,
-	"NgayTao" Timestamp,
-	"TextLog" Text,
-	"Platforfrfrm" Varchar(250),
-	"UngDung" Varchar(250),
-	"TieuDeLog" Varchar(250),
- primary key ("IDLog")
-) Without Oids;
-
-
-
-
+/*
+Created		20-Oct-16
+Modified		08-Dec-16
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
+*/
+/*
+Created		20-Oct-16
+Modified		08-Dec-16
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
+*/
